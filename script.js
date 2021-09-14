4b9335b1a27363fc664e53a2ed8814ebef55893d
Rafael Porto <rafaelgportof@gmail.com>
2021-09-14 08:06:45 -0300

    Soma e Subtração

---
+++ script.js
@@ -0,0 +1,18 @@
+function soma(){
+    var operator1 = document.getElementById('calc1');
+    var operator2 = document.getElementById('calc2');
+    var res = document.getElementById('resSoma');
+    var namber1 = Number(operator1.value);
+    var number2 = Number(operator2.value);
+    var result = namber1 + number2;
+    res.innerHTML = `${namber1} + ${number2} = ${result}`;
+}
+function subtracao(){
+    var operator1 = document.getElementById('sub1');
+    var operator2 = document.getElementById('sub2');
+    var res = document.getElementById('resSub');
+    var number1 = Number(operator1.value);
+    var number2 = Number(operator2.value);
+    var result = number1 - number2;
+    res.innerHTML = `${number1} - ${number2} = ${result}`;
+}
