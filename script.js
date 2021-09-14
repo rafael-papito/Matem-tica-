3cf1b0e9bf74843ae0fd5c0bea2ccd02766c2d5e
Rafael Porto <rafaelgportof@gmail.com>
2021-09-14 08:09:05 -0300

    Divisão e multiplicação

--- script.js
+++ script.js
@@ -16,3 +16,21 @@ function subtracao(){
     var result = number1 - number2;
     res.innerHTML = `${number1} - ${number2} = ${result}`;
 }
+function division(){
+    var operator1 = document.getElementById('div1');
+    var operator2 = document.getElementById('div2');
+    var res = document.getElementById('resDiv');
+    var number1 = Number(operator1.value);
+    var number2 = Number(operator2.value);
+    var result = number1 / number2;
+    res.innerHTML = `${number1} / ${number2} = ${result}`;
+}
+function multiplicacao(){
+    var operator1 = document.getElementById('mult1');
+    var operator2 = document.getElementById('mult2');
+    var res = document.getElementById('resMult');
+    var number1 = Number(operator1.value);
+    var number2 = Number(operator2.value);
+    var result = number1 * number2;
+    res.innerHTML = `${number1} * ${number2} = ${result}`;
+}
